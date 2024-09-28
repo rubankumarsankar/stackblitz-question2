571c33e241dbcd5b7667079845b13171183b9017
rubankumarsankar <109938763+rubankumarsankar@users.noreply.github.com>
2024-09-28 05:45:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-question2
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/rubankumarsankar/stackblitz-question2)
---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.menu-icon').addEventListener('click', function () {
+  const nav = document.querySelector('nav ul');
+  nav.classList.toggle('show');
+});
